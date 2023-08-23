494b8d1c98a0966831bda1eeca05c01a5ecba911
Bruno Claudino <bruarrais@gmail.com>
2023-08-22 22:18:19 -0300

    add cloudflare dns

--- nixos/inix/configuration.nix
+++ nixos/inix/configuration.nix
@@ -36,6 +36,9 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # Change DNS
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001"];
+
   # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
 
