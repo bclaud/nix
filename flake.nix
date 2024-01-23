23aa0a7f4f2d4edb57c88adf88ee54aaf0eda926
Bruno Claudino <bruarrais@gmail.com>
2024-01-23 18:17:04 -0300

    remove hm derivation

--- flake.nix
+++ flake.nix
@@ -51,21 +51,5 @@
         ];
       };
     };
-
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-
-      "nclaud@inix" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = { inherit inputs unison-nix; }; # Pass flake inputs to our config
-        # > Our main home-manager configuration file <
-        modules = [ 
-          ./home-manager/home.nix
-          hyprland.homeManagerModules.default
-          {wayland.windowManager.hyprland.enable = true;}
-         ];
-      };
-    };
   };
 }
