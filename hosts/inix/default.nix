2d8e39f6e5cd43b358aa3ad4792cf1faba539f1e
Bruno Claudino <bruarrais@gmail.com>
2024-01-26 14:39:33 -0300

    move sound settings to desktops

--- hosts/inix/default.nix
+++ hosts/inix/default.nix
@@ -52,37 +52,23 @@
   };
 
 
-  services.jellyfin = {
-    enable = true;
-    user = "nclaud";
-    openFirewall = true;
+  services = {
+    jellyfin = {
+      enable = true;
+      user = "nclaud";
+      openFirewall = true;
+    };
+
+    languagetool = {
+      enable = false;
+      allowOrigin = "*";
+    };
+
+    i2p = {
+      enable = false;
+    };
   };
 
-  services.languagetool = {
-    enable = false;
-    allowOrigin = "*";
-  };
-
-  services.i2p = {
-    enable = false;
-  };
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   users.users.nclaud = {
     isNormalUser = true;
