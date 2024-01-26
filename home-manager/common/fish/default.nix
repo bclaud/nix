ca55b0c98f9f8532f0d181b02ff20ccfef31cb77
Bruno Claudino <bruarrais@gmail.com>
2024-01-26 12:49:01 -0300

    fish refactor
    fish

--- home-manager/common/fish/default.nix
+++ home-manager/common/fish/default.nix
@@ -1,8 +1,75 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [fish zoxide starship ripgrep fd];
+{pkgs, lib, config, ...}: 
+let
+  packageNames = map (p: p.pname or p.name or null) config.home.packages;
+  hasPackage = name: lib.any (x: x == name) packageNames;
+  hasLazydocker = hasPackage "lazydocker";
+  hasLazygit = hasPackage "lazygit";
+  hasZellij = hasPackage "zellij";
+in
+{
+  home.packages = with pkgs; [
+    fd 
+    fzf
+    ripgrep
+    ];
 
-  xdg.configFile.fish = {
-    source = ./config;
-    recursive = true;
+  programs.fish = {
+    enable = true;
+
+    shellAliases = {
+      mt = "mix test";
+      g = "git";
+      n = "nvim .";
+
+      lzd = lib.mkIf hasLazydocker "lazydocker";
+      lzg = lib.mkIf hasLazygit "lazygit";
+
+
+      zj = lib.mkIf hasZellij "zellij";
+      zje = lib.mkIf hasZellij "zellij run -- nvim .";
+
+      ",," = "nix_run";
+      ",." = "nix_shell_fish";
+    };
+
+    
+    functions = {
+      fish_greeting = "";
+
+      nix_run = "nix run nixpkgs#argv";
+
+      nix_shell_fish = "nix shell nixpkgs#argv -c fish";
+
+      pythonEnv = {
+        description = "start a nix-shell with given python packages";
+        argumentNames = ["pythonVersion"];
+        body = ''
+        if set -q argv[2]
+          set argv $argv[2..-1]
+        end
+      
+        for el in $argv
+          set ppkgs $ppkgs "python"$pythonVersion"Packages.$el"
+        end
+      
+        nix-shell -p $ppkgs
+        '';
+      };
+
+    };
+
+    # TODO this should be handled by yubikey-agent
+    interactiveShellInit = ''
+      set -x GPG_TTY (tty)
+      set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+      gpgconf --launch gpg-agent
+    '';
+  };
+
+  programs.starship.enable = true;
+
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
   };
 }
