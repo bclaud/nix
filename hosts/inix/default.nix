6e4a8ab7735b5d87b5d154b81d0b1b961adf7532
Bruno Claudino <bruarrais@gmail.com>
2024-01-28 14:30:24 -0300

    update

--- hosts/inix/default.nix
+++ hosts/inix/default.nix
@@ -75,7 +75,7 @@
 
   networking = {
     hostName = "inix"; # Define your hostname.
-    networkmanager.enable = true;
+    useDHCP = true;
     nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001"];
   };
 
