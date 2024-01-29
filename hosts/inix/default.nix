631cc81ba6450de76a4c69d7fd5e17539c0c07bd
Bruno Claudino <bruarrais@gmail.com>
2024-01-28 21:40:34 -0300

    use dhcp

--- hosts/inix/default.nix
+++ hosts/inix/default.nix
@@ -75,8 +75,8 @@
 
   networking = {
     hostName = "inix"; # Define your hostname.
-    networkmanager.enable = true;
-    nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001"];
+    networkmanager.enable = false;
+    useDHCP = true;
   };
 
   users = {
