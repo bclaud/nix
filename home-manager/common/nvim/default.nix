eb5d4ce3f8763adf16788b2e953b1f4ccd33a0a9
Bruno Claudino <bruarrais@gmail.com>
2024-08-14 07:43:01 -0300

    nvim config

--- home-manager/common/nvim/default.nix
+++ home-manager/common/nvim/default.nix
@@ -352,6 +352,7 @@
       end,
     },
     mapping = cmp.mapping.preset.insert {
+      ['<C-y>'] = cmp.mapping.confirm { select = true },
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
@@ -381,6 +382,7 @@
     sources = {
       { name = 'nvim_lsp' },
       { name = 'luasnip' },
+      { name = 'path' },
     },
   }
 
